f12c17b260af3e9c4690a572858ddc4db2688d5a
bestlalala <bestdustn@gmail.com>
2022-01-11 23:56:57 +0900

    step6 functions

---
+++ Baekjoon/src/Step6_function/P4673.java
@@ -0,0 +1,25 @@
+package Step6_function;
+
+public class P4673 {
+
+    public static int d(int n) {
+        n = n + n/10 + n%10;
+        return n;
+    }
+
+    public static void main(String[] args) {
+        boolean[] check = new boolean[10001];
+
+        for (int i = 1; i <= 10000; i++) {
+            int n = d(i);
+
+            if (n <= 10000)
+                check[n] = true;
+        }
+
+        for (int i = 1; i < check.length; i++) {
+            if (!check[i])
+                System.out.println(i);
+        }
+    }
+}
