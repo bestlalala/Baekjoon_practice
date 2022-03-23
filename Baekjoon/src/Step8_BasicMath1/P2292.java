f884d613dc278377efb5860690b1d9f78bc3c482
bestlalala <bestdustn@gmail.com>
2022-03-23 16:49:38 +0900

    벌집

--- Baekjoon/src/Step8_BasicMath1/P2292.java
+++ Baekjoon/src/Step8_BasicMath1/P2292.java
@@ -1,2 +1,19 @@
-package Step8_BasicMath1;public class P2292 {
+package Step8_BasicMath1;
+
+import java.util.Scanner;
+
+// 벌집
+public class P2292 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int room = 1;
+        int x = 1;
+        for (int i = 1; n > x ; i++ ) {
+            x += i*6;
+            room++;
+        }
+        System.out.println(room);
+    }
 }
