5477318edf2bf7edcde43f8ab45bfbd3e661944a
bestlalala <bestdustn@gmail.com>
2022-03-23 16:45:40 +0900

    단어의 개수

--- Baekjoon/src/Step7_String/P1152.java
+++ Baekjoon/src/Step7_String/P1152.java
@@ -1,2 +1,17 @@
-package Step7_String;public class P1152 {
+package Step7_String;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 단어의 개수
+public class P1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        StringTokenizer tokenizer = new StringTokenizer(str, " ");
+        int count = tokenizer.countTokens();
+
+        System.out.println(count);
+    }
 }
