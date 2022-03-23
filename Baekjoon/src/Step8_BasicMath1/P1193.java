33880eaf612553fc34076baf2ffa03f2be91faa2
bestlalala <bestdustn@gmail.com>
2022-03-23 16:46:51 +0900

    분수 찾기

--- Baekjoon/src/Step8_BasicMath1/P1193.java
+++ Baekjoon/src/Step8_BasicMath1/P1193.java
@@ -1,2 +1,35 @@
-package Step8_BasicMath1;public class P1193 {
+package Step8_BasicMath1;
+
+import java.util.Scanner;
+
+// 분수 찾기
+public class P1193 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        int n = 1, a = 1, b = 1; // n번째 분수 a/b
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < i; j++) {
+                    if (n == x) break;
+                    a++;
+                    if (b != 1) b--;
+                    n++;
+//                    System.out.println(n+"번째- "+ "i:"+i + ", j:"+j+" "+ a+"/"+b);
+                }
+            }
+            else {
+                for (int j = 0; j < i; j++) {
+                    if (n == x) break;
+                    b++;
+                    if (a != 1) a--;
+                    n++;
+//                    System.out.println(n+"번째- "+ "i:"+i + ", j:"+j+" "+ a+"/"+b);
+                }
+            }
+        }
+        System.out.println(a + "/" + b);
+
+    }
 }
