7e77ffa0bf46600035a2ea49a3b942f4c652f352
bestlalala <bestdustn@gmail.com>
2022-03-23 16:52:00 +0900

    크로아티아 알파벳

--- Baekjoon/src/Step7_String/P2908.java
+++ Baekjoon/src/Step7_String/P2908.java
@@ -1,2 +1,30 @@
-package Step7_String;public class P2908 {
+package Step7_String;
+
+import java.util.Scanner;
+
+// 상수
+public class P2908 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+
+        String snum1 ="";
+        for (int i = str1.length(); i > 0 ; i--) {
+            snum1 += str1.charAt(i-1);
+        }
+        String snum2 ="";
+        for (int i = str2.length(); i > 0 ; i--) {
+            snum2 += str2.charAt(i-1);
+        }
+
+        int num1 = Integer.parseInt(snum1);
+        int num2 = Integer.parseInt(snum2);
+
+        if (num1 < num2)
+            System.out.println(num2);
+        else
+            System.out.println(num1);
+
+    }
 }
--- Baekjoon/src/Step7_String/P2941.java
+++ Baekjoon/src/Step7_String/P2941.java
@@ -1,2 +1,20 @@
-package Step7_String;public class P2941 {
+package Step7_String;
+
+import java.util.Scanner;
+
+// 크로아티아 알파벳
+public class P2941 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
+
+        String croatia[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (int i = 0; i < croatia.length; i++) {
+            if (str.contains(croatia[i]))
+                str = str.replace(croatia[i], "!");
+        }
+        System.out.println(str.length());
+    }
 }
