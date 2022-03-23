bc5191d13b9de67501d260e82be1dd0798809f91
bestlalala <bestdustn@gmail.com>
2022-03-23 16:48:27 +0900

    손익분기점

--- Baekjoon/src/Step8_BasicMath1/P1712.java
+++ Baekjoon/src/Step8_BasicMath1/P1712.java
@@ -1,2 +1,23 @@
-package Step8_BasicMath1;public class P1712 {
+package Step8_BasicMath1;
+
+import java.util.Scanner;
+
+// 손익분기점
+public class P1712 {
+    public static void main(String[] args) {
+        int n;
+        int A, B, C;
+
+        Scanner scanner = new Scanner(System.in);
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = scanner.nextInt();
+
+        if (B >= C)
+            System.out.println(-1);
+        else {
+            n = A / (C - B) + 1;
+            System.out.println(n);
+        }
+    }
 }
