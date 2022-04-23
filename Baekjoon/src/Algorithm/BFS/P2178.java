641165af79bc3e28eca8aa267d7d3eb4bf765b9b
bestlalala <bestdustn@gmail.com>
2022-04-23 21:09:30 +0900

    BOJ 2178번 BFS  미로탐색 문제풀이

--- Baekjoon/src/Algorithm/BFS/P2178.java
+++ Baekjoon/src/Algorithm/BFS/P2178.java
@@ -1,5 +1,7 @@
 package Algorithm.BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 2178번: 미로 탐색
@@ -8,9 +10,18 @@ public class P2178 {
     static int m;
     static int[][] maze;
     static boolean[][] visited;
-    static int count = 0;
+    static Queue<Location> queue;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
+
+    static class Location {
+        int row, col, dist;
+        public Location(int row, int col, int dist) {
+            this.row = row;
+            this.col = col;
+            this.dist = dist;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,6 +30,7 @@ public class P2178 {
 
         maze = new int[n][m];
         visited = new boolean[n][m];
+        queue = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
             String str = sc.next();
@@ -26,31 +38,32 @@ public class P2178 {
                 maze[i][j] = str.charAt(j) - '0';
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (maze[i][j] == 1 &&!visited[i][j]) {
-                    move(i, j);
-                }
-            }
-        }
-        System.out.println(count);
+        bfs();
+        sc.close();
     }
 
-    public static void move(int x, int y) {
-        visited[x][y] = true;
+    public static void bfs() {
+        queue.add(new Location(0, 0, 1));
+        visited[0][0] = true;
+        while (!queue.isEmpty()) {
+            Location location = queue.poll();
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nr = location.row + dy[i];
+                int nc = location.col + dx[i];
 
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                if (maze[nx][ny] == 1 && !visited[nx][ny]) {
-                    move(nx, ny);
-                    count++;
+                if (location.row == n-1 && location.col == m-1) {
+                    System.out.println(location.dist);
+                    return;
+                }
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if (maze[nr][nc] == 1 && !visited[nr][nc]) {
+                        queue.add(new Location(nr, nc, location.dist+1));
+                        visited[nr][nc] = true;
+                    }
                 }
             }
         }
-
     }
 }
