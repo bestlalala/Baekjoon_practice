d26c1e1bdaf7f844075b284112ba309d04032572
bestlalala <bestdustn@gmail.com>
2022-04-24 18:53:24 +0900

    백준 10250번 ACM 호텔

--- Baekjoon/src/Step8_BasicMath1/P10250.java
+++ Baekjoon/src/Step8_BasicMath1/P10250.java
@@ -1,8 +1,32 @@
 package Step8_BasicMath1;
 
+import java.util.Scanner;
+
 // ACM 호텔
 public class P10250 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
+        for (int i = 0; i < t; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+            test(h, w, n);
+        }
+        sc.close();
+    }
+
+    public static void test(int h, int w, int n) {
+        int floor, room;
+        if (n%h == 0) {
+            floor = h;
+            room = n / h;
+        }
+        else {
+            floor = n % h;
+            room = n / h + 1;
+        }
+        System.out.println(floor*100 + room);
     }
 }
