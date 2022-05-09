ec3a1f472986d4f3c9fe394b75f9e2f6fa336c1f
bestlalala <bestdustn@gmail.com>
2022-05-10 00:27:30 +0900

    2606번BFS - 스스로 해내기 성공

--- Baekjoon/src/Algorithm/BFS/P2606.java
+++ Baekjoon/src/Algorithm/BFS/P2606.java
@@ -6,6 +6,7 @@ public class P2606 {
     static int count = 0;
     static LinkedList<Integer>[] computers;
     static boolean[] visited;
+    static Queue<Integer> queue;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,6 +15,7 @@ public class P2606 {
 
         computers = new LinkedList[n+1];
         visited = new boolean[n+1];
+        queue = new LinkedList<>();
 
         for (int i = 0; i <= n; i++) {
             computers[i] = new LinkedList<>();
@@ -29,15 +31,27 @@ public class P2606 {
         for (int i = 0; i <= n; i++) {
             Collections.sort(computers[i]);
         }
-        virus(1);
 
-        System.out.println(count);
+        queue.add(1);
+        while(!queue.isEmpty()) {
+            int com = queue.poll();
+            if (!visited[com])
+                virus(com);
+        }
+
+        System.out.println(count-1);
     }
 
     public static void virus(int computer) {
         visited[computer] = true;
+        count++;
 
-
-
+        Iterator<Integer> iter = computers[computer].listIterator();
+        while(iter.hasNext()) {
+            int w = iter.next();
+            if (!visited[w]&&!queue.contains(w)) {
+                queue.add(w);
+            }
+        }
     }
 }
